14ef57eb4b8e79d003f2665654ce0df707c8ba60
Paulo Sergio <pauloserg861@gmail.com>
2026-08-05 21:28:13 -0300

    feat: add and config pinia

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,10 @@ import './style.css'
 import App from './App.vue'
 import { router } from './routes'
 import { i18n } from './i18n'
+import { pinia } from './stores'
 
 const app = createApp(App)
+app.use(pinia)
 app.use(router)
 app.use(i18n)
 
---
+++ src/stores/auth.ts
@@ -0,0 +1,12 @@
+import { defineStore } from 'pinia'
+import { ref } from 'vue'
+
+export const useAuthStore = defineStore('auth', () => {
+  const token = ref<string | null>(null)
+  return { token }
+}, {
+  persist: {
+    pick: ['token'],
+    storage: localStorage,
+  },
+})
